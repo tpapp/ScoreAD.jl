ddc548859f6e450641967eef5e97c32e4395afe3
Tamas K. Papp <tkpapp@gmail.com>
2017-11-15 15:03:44 +0100

    Code and tests.

--- src/ScoreAD.jl
+++ src/ScoreAD.jl
@@ -1,5 +1,34 @@
 module ScoreAD
 
-# package code goes here
+import ForwardDiff: Dual, partials
+
+export score_AD, score_AD_log
+
+"""
+    score_AD(p)
+
+When `p` is real, return `1` (with the same type).
+
+When `p` is a primitive for automatic differentiation, return ``log ∂p``, ie the
+score function.
+
+This function can be used for gradient estimation using simulation. See the
+references in the README.
+"""
+score_AD(p::Real) = one(p)
+
+score_AD(p::Dual{T,V}) where {T,V} = Dual{T}(one(V), partials(log(p)))
+
+"""
+    score_AD_log(ℓ)
+
+Similar to [`score_AD`](@ref), but uses the log likelihood, ie
+```julia
+score_AD(p) == score_AD_log(log(p))
+```
+"""
+score_AD_log(ℓ::Real) = one(ℓ)
+
+score_AD_log(ℓ::Dual{T,V}) where {T,V} = Dual{T}(one(V), partials(ℓ))
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,56 @@
-using ScoreAD
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+using ScoreAD
+
+using Distributions
+import ForwardDiff: derivative
+
+"""
+    BernoulliData(N, s)
+
+`N` draws from a Bernoulli(θ) distribution, `s` of which are `1`, `N-s` are `0`.
+"""
+struct BernoulliData
+    "Total number of observations."
+    N::Int
+    "Observations =1."
+    s::Int
+end
+
+(data::BernoulliData)(θ) = data.s/data.N*score_AD(θ)
+
+@testset "bernoulli" begin
+    N = 105
+    s = 61
+    data = BernoulliData(N, s)
+    θ = 0.5
+    for θ in linspace(0.1, 0.9, 100)
+        @test data(θ) ≈ s/N
+        @test derivative(data, θ) ≈ s/N*1/θ
+    end
+end
+
+"""
+    NormalData(x)
+
+A vector of draws from a Normal(0, σ) distribution.
+"""
+struct NormalData{T}
+    x::Vector{T}
+end
+
+function (data::NormalData)(σ)
+    d = Normal(0.0, σ)
+    mean(x * score_AD_log(logpdf(d, x)) for x in data.x)
+end
+
+σ = 1.0
+x = rand(Normal(0, σ), 100)
+data = NormalData(x)
+
+@testset "normal" begin
+    for σ in linspace(0.1, 5, 100)
+        @test data(σ) ≈ mean(x)
+        @test derivative(data, σ) ≈ mean(x * (x^2/(σ^3) - 1/σ) for x in x)
+    end
+end
